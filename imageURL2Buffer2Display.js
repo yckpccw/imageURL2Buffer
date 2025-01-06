c02b272a8eaa13a585ed549647c6c67fbad52528
yckpccw <Leax.HY.Chui@pccw.com>
2025-01-06 17:10:28 +0800

    Update imageURL2Buffer2Display.js

--- imageURL2Buffer2Display.js
+++ imageURL2Buffer2Display.js
@@ -1,4 +1,4 @@
-<div id="imageDiv"></div>
+// <div id="imageDiv"></div>
 
 <script>
     async function loadImage(url, divId) {
@@ -12,6 +12,6 @@
     }
 
     // Example usage
-    const imageUrl = 'https://example.com/path/to/your/image.jpg'; // Replace with your image URL
+    const imageUrl = 'http://10.94.11.163/zabbix/chart.php?from=now-3h&to=now&itemids%5B0%5D={ITEM.ID}&type=0&resolve_macros=1&profileIdx=web.item.graph.filter&profileIdx2={ITEM.ID}&width=1600&height=200'; // Replace with your image URL
     loadImage(imageUrl, 'imageDiv'); // Show image in imageDiv tab
 </script>
